1c86a420733be53329d00693e4a1d461c0a9fda1
beni-f <benifissha@gmail.com>
2024-06-14 22:48:59 +0300

    updated

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """
-	1-concurrent_coroutines
+1-concurrent_coroutines
 """
 import asyncio
-import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n, max_delay):
+
+async def wait_n(n: int, max_delay: int):
     """
         Spawn wait_random n times with the specified max_delay
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
-    results = await asyncio.gather(*tasks)	
+    results = await asyncio.gather(*tasks)
 
     sorted_results = []
     while results:
