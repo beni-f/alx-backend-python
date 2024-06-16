e71ceea7a5625f5ebb6ff3bf8c302bba32821918
beni-f <benifissha@gmail.com>
2024-06-16 18:15:53 +0300

    2-measure_runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Run time for four parallel comprehensions
+"""
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+import asyncio
+import time
+
+
+async def measure_runtime():
+    start_time = time.perf_counter()
+    asyncComprehension = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*asyncComprehension)
+    end_time = time.perf_counter()
+    return end_time - start_time
