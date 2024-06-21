807a7591fcc6b946b46d9f56ce6639874d29ce9d
beni-f <benifissha@gmail.com>
2024-06-21 16:22:59 +0300

    2-add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,5 +2,11 @@
 """
     Basic annotations - add
 """
+
+
 def add(a: float, b: float) -> float:
+    """
+        Takes a float a and a float b as\
+        arguments and returns their sum as a float
+    """
     return a + b
