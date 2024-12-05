4051fb91be2cb7350a19b0bd5024f10c3b918d8c
beni-f <benifissha@gmail.com>
2024-12-05 22:33:58 +0300

    Generator that streams rows from an SQL database

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,18 @@
+import csv
+import uuid
+from itertools import islice
+
+def stream_users():
+    with open('user_data.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            user_id = uuid.uuid4()
+            yield {
+                'user_id': str(user_id),
+                'name': row['name'],
+                'email': row['email'],
+                'age': row['age'],
+            }
+
+for user in islice(stream_users(), 6):
+    print(user)
