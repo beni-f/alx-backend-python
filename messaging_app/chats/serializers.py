e6fc084815bef62f548ffbccb57c5011e22bb81f
beni-f <benifissha@gmail.com>
2024-12-26 23:57:41 +0300

    updated

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -7,11 +7,26 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True)
     class Meta:
         model = Conversation
-        fields = '__all__'
+        fields = ['conversation_id', 'participants']
 
 class MessageSerializer(serializers.ModelSerializer):
+    conversation = ConversationSerializer()
+    sender = UserSerializer()
     class Meta:
         model = Message
-        fields = '__all__'
+        fields = ['message_id', 'conversation', 'sender']
+
+    def create(self, validated_data):
+        conversation_data = validated_data.pop('conversation')
+        sender_data = validated_data.pop('sender')
+
+        conversation, _ = Conversation.objects.get_or_create(**conversation_data)
+
+        sender, _ = User.objects.get_or_create(**sender_data)
+
+        message = Message.objects.create(conversation=conversation, sender=sender, **validated_data)
+
+        return message
