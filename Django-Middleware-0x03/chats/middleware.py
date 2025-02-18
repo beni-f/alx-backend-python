96b69bf86f2cfe449da59edec06503ee286d033d
beni-f <benifissha@gmail.com>
2025-02-18 12:55:13 +0300

    Detect and Block offensive language

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,7 @@
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.http import HttpResponseForbidden
+from collections import defaultdict
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -27,3 +28,23 @@ class RestrictAccessByTimeMiddleware:
             return response
         else:
             return HttpResponseForbidden("Access restricted at this time of the day")
+        
+class OffensiveLanguageMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.message_count = defaultdict(list)
+
+    def __call__(self, request, *args, **kwds):
+        ip_address = request.META.get('REMOTE_ADDR')
+        current_time = datetime.now()
+
+        if request.path == '/api/message/' and request.method == 'POST':
+            self.message_count[ip_address] = [time for time in self.message_count[ip_address] if current_time - time < timedelta(minutes=1)]
+
+            if len(self.message_count[ip_address]) >= 5:
+                return HttpResponseForbidden("You have exceeded the message limit")
+
+            self.message_count[ip_address].append(current_time)
+
+        response = self.get_response(request)
+        return response
